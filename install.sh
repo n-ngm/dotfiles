d468d4b7854371ba4f73b81fc699398806fd2976
clickmaker <n_nagami@clickmaker.jp>
2026-02-01 12:46:45 +0900

    refactor: move Brewfile to hidden directory and update install script to link it

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -snf $SCRIPT_DIR/.vimrc     .vimrc
 ln -snf $SCRIPT_DIR/.zshrc     .zshrc
 ln -snf $SCRIPT_DIR/.alias     .alias
 ln -snf $SCRIPT_DIR/.p10k.zsh  .p10k.zsh
+ln -snf $SCRIPT_DIR/.Brewfile  .Brewfile
 
 cd $HOME
 mkdir -p .config/nvim
